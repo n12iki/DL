e418ce7727404751c24aca44e9a841e75a13d8be
demetere <demetredzmanashvili@gmail.com>
2022-03-30 22:37:29 +0200

    Latest modifiicatiosn on entire structure of model, data_loader and trainer

--- MAMeProject/data_loader.py
+++ MAMeProject/data_loader.py
@@ -1,14 +1,12 @@
 import tensorflow as tf
-# from sklearn.utils import shuffle
-import cv2
-from cv2 import imread, resize
-#from tensorflow.keras import utils
+from keras.preprocessing.image import ImageDataGenerator
+from sklearn.utils import shuffle
 import math
 import pandas as pd
 import numpy as np
-import os
-#from collections import deque
-#import copy
+import random
+
+per_sample_normalization = True
 
 class MAMeDataset(tf.keras.utils.Sequence):
     def __init__(self, batch_size, n_class, mode='train'):
@@ -20,6 +18,7 @@ class MAMeDataset(tf.keras.utils.Sequence):
         self.images = ['.'.join(img.split(".")[:-1]) for img in self.images]
         self.batch_size = batch_size
         self.n_class = n_class
+        self.mode = mode
         
     def __len__(self):
         return math.ceil(len(self.images) / self.batch_size)
@@ -48,118 +47,30 @@ class MAMeDataset(tf.keras.utils.Sequence):
         data_x = np.array(data_x, dtype='float32')
         data_y = np.array(data_y)
         
-        return data_x, data_y
+
+        # augmentation
+        if self.mode == 'train':
+            seed = np.random.randint(1, 25, 1)
+
+            data_x = tf.image.random_flip_left_right(data_x, seed)
+            data_x = tf.image.random_flip_up_down(data_x, seed)
+            data_x = tf.image.random_brightness(data_x, .5, seed)
+            data_x = tf.image.random_contrast(data_x, 0, 2, seed)
+            # data_x = tf.image.random_hue(data_x, 0.5, seed)
+
+
+            data_x /= 255
+
+            data_x = tf.image.central_crop(data_x, random.choice(np.arange(.5, 1, .1)))
+            data_x = tf.image.resize(data_x, (256,256), tf.image.ResizeMethod.BILINEAR)
+
+        else: 
+            data_x /= 255
+
+        return (data_x, data_y)
     
-    # def on_epoch_end(self):
-    #     # option method to run some logic at the end of each epoch: e.g. reshuffling
-    #     seed = np.random.randint()
-    #     self.x = shuffle(self.x, random_state=seed)
-    #     self.y = shuffle(self.y, random_state=seed)
+    def on_epoch_end(self):
+        # option method to run some logic at the end of each epoch: e.g. reshuffling
+        self.images, self.labels = shuffle(self.images, self.labels, random_state=42)
 
-# class ActionDataGenerator(object):
-    
-#     def __init__(self,root_data_path,temporal_stride=1,temporal_length=16,resize=224, max_sample=20):
-        
-#         self.root_data_path = root_data_path
-#         self.temporal_length = temporal_length
-#         self.temporal_stride = temporal_stride
-#         self.resize=resize
-#         self.max_sample=max_sample
 
-#     def file_generator(self,data_path,data_files):
-#         '''
-#         data_files - list of csv files to be read.
-#         '''
-#         for f in data_files:       
-#             tmp_df = pd.read_csv(os.path.join(data_path,f))
-#             label_list = list(tmp_df['Label'])
-#             total_images = len(label_list) 
-#             if total_images>=self.temporal_length:
-#                 num_samples = int((total_images-self.temporal_length)/self.temporal_stride)+1
-                
-#                 img_list = list(tmp_df['FileName'])
-#             else:
-#                 print ('num of frames is less than temporal length; hence discarding this file-{}'.format(f))
-#                 continue
-            
-#             samples = deque()
-#             samp_count=0
-#             for img in img_list:
-#                 if samp_count == self.max_sample:
-#                     break
-#                 samples.append(img)
-#                 if len(samples)==self.temporal_length:
-#                     samples_c=copy.deepcopy(samples)
-#                     samp_count+=1
-#                     for t in range(self.temporal_stride):
-#                         samples.popleft()
-#                     yield samples_c,label_list[0]
-
-#     def load_samples(self,data_cat='train', test_ratio=0.1):
-#         data_path = os.path.join(self.root_data_path,data_cat)
-#         csv_data_files = os.listdir(data_path)
-#         file_gen = self.file_generator(data_path,csv_data_files)
-#         iterator = True
-#         data_list = []
-#         while iterator:
-#             try:
-#                 x,y = next(file_gen)
-#                 x=list(x)
-#                 data_list.append([x,y])
-#             except Exception as e:
-#                 print ('the exception: ',e)
-#                 iterator = False
-#                 print ('end of data generator')
-#         # data_list = self.shuffle_data(data_list)
-#         return data_list
-    
-#     def train_validation_split(self, data_list, target_column, val_size=0.1, ks_sequence=False):
-#         dataframe = pd.DataFrame(data_list)
-#         dataframe.columns = ['Feature', target_column]
-#         data_dict = dict()
-#         for i in range(len(np.unique(dataframe[target_column]))):
-#             data_dict[i] = dataframe[dataframe[target_column]==i]
-#         train, validation = pd.DataFrame(), pd.DataFrame()
-#         for df in data_dict.values():
-#             cut = int(df.shape[0] * val_size)
-#             val = df[:cut]
-#             rem = df[cut:]
-#             train = train.append(rem, ignore_index=True)
-#             validation = validation.append(val, ignore_index=True)
-#         if ks_sequence:
-#             return train['Feature'].values.tolist(), train['Label'].values.tolist(), \
-#                 validation['Feature'].values.tolist(), validation['Label'].values.tolist() # without shuffle
-#         return train.values.tolist(), validation.values.tolist() # without shuffle
-
-# root_data_path = 'C:\\Users\\AI-lab\\Documents\\activity_file\\UCF101\\csv_files\\' # machine specific
-# CLASSES = 101
-# BATCH_SIZE = 10
-# EPOCHS = 1
-# TEMPORAL_STRIDE = 8
-# TEMPORAL_LENGTH = 16
-# MAX_SAMPLE = 20
-# HEIGHT = 192
-# WIDTH = 256
-# CHANNEL = 3
-
-# data_gen_obj = ActionDataGenerator(root_data_path, temporal_stride=TEMPORAL_STRIDE, \
-#                                   temporal_length=TEMPORAL_LENGTH, max_sample=MAX_SAMPLE)
-# train_data = data_gen_obj.load_samples(data_cat='train')
-# x_train, y_train, x_val, y_val = data_gen_obj.train_validation_split(train_data, 'Label', 0.1, True)
-# r1 = MEMaDataset(x_train, y_train, BATCH_SIZE, CLASSES)
-# r2 = MEMaDataset(x_val, y_val, BATCH_SIZE, CLASSES)
-# print(type(r1), type(r2))
-
-# model = tf.keras.models.Sequential()
-# model.add(tf.keras.layers.Conv3D(10, input_shape=(TEMPORAL_LENGTH,HEIGHT,WIDTH,CHANNEL), kernel_size=(2,2,2), strides=2))
-# model.add(tf.keras.layers.Conv3D(10, kernel_size=(2,3,3), strides=2))
-# model.add(tf.keras.layers.Conv3D(10, kernel_size=(2,3,3), strides=2))
-# model.add(tf.keras.layers.Conv3D(10, kernel_size=(2,3,3), strides=2))
-# model.add(tf.keras.layers.Flatten())
-# model.add(tf.keras.layers.Dense(101, activation='softmax'))
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# model.summary()
-
-# train_history = model.fit(r1, epochs=3, steps_per_epoch=r1.__len__(), verbose=1)
-# score = model.evaluate(r2, steps=5)
-# print(score)
--- MAMeProject/model.py
+++ MAMeProject/model.py
@@ -3,62 +3,60 @@ import keras as k
 from tensorflow.keras.models import Model
 from keras.models import Sequential
 from keras import regularizers
-from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, GlobalAveragePooling2D
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, GlobalAveragePooling2D, Input, add
 from keras.layers import Conv2D, MaxPooling2D
 from tensorflow.keras import optimizers
 from data_loader import MAMeDataset
 import matplotlib.pyplot as plt
 
+# from keras.utils.vis_utils import plot_model
+
 img_size = 256
 globalAVGPooling = False
-num_classes = 20
 weight_decay = 1e-4
-loss = ['categorical_crossentropy', 'binary_crossentropy', 'mean_squared_error', 'mean_absolute_error'][0]
-n_epochs = 5
-batch_size = 128
+last_layer_activation = ['softmax', 'sigmoid', None][0]
 num_classes = 29
 
+def block(numConv,layer_in,n_filters,dropOutR,weight_decay):
+    merge_input=layer_in
+    if layer_in.shape[-1]!=n_filters:
+        merge_input=Conv2D(n_filters,(1,1),padding='same',activation='relu',kernel_initializer='he_normal',kernel_regularizer=regularizers.l2(weight_decay))(layer_in)
+    conv1=Conv2D(n_filters,(3,3),padding="same",activation="relu",kernel_initializer="he_normal",kernel_regularizer=regularizers.l2(weight_decay))(layer_in)
+    for i in range(numConv-1):
+        prev=conv1
+        conv1=Dropout(dropOutR)(conv1)
+        conv1=Conv2D(n_filters,(3,3),padding="same",activation="relu",kernel_initializer="he_normal",kernel_regularizer=regularizers.l2(weight_decay))(conv1)
+        conv1=add([conv1,prev])
+        #conv1=add([conv1,merge_input])    
+    conv1=Dropout(dropOutR)(conv1)
+    skip=add([conv1,merge_input])
+    layer_out=MaxPooling2D(pool_size=(2,2))(skip)
+    layer_out = Activation('relu')(layer_out)
+    return layer_out
+
 def create_model():
-  model = Sequential()
-  model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay), input_shape=(img_size, img_size, 3)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-
-  model.add(Conv2D(32, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-  model.add(MaxPooling2D(pool_size=(2,2)))
-
-  model.add(Dropout(0.2))
-  
-  model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-
-  model.add(Conv2D(64, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-  model.add(MaxPooling2D(pool_size=(2,2)))
-
-  model.add(Dropout(0.3))
-  
-  model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-
-  model.add(Conv2D(128, (3,3), padding='same', kernel_regularizer=regularizers.l2(weight_decay)))
-  model.add(Activation('relu'))
-  model.add(BatchNormalization())
-  model.add(MaxPooling2D(pool_size=(2,2)))
-
-  model.add(Dropout(0.4))
+  visible = Input(shape=(256, 256, 3))
+  layer=block(3,visible,16,0.2,weight_decay)
+  layer=block(2,layer,32,0.3,weight_decay)
+  layer=block(3,layer,64,0.3,weight_decay)
+  layer=block(2,layer,128,0.3,weight_decay)
+  layer=block(3,layer,256,0.3,weight_decay)
+  layer=block(2,layer,256,0.3,weight_decay)
+  layer=block(2,layer,512,0.3,weight_decay)
 
   if globalAVGPooling:
-    model.add(GlobalAveragePooling2D())
+    layer=GlobalAveragePooling2D()(layer)
   else:
-    model.add(Flatten())
+    layer=Flatten()(layer)
 
-  model.add(Dense(num_classes, activation='softmax'))
-  
+  # layer=Dense(2048)(layer)
+  # layer=Dense(1024)(layer)
+  layer=Dense(num_classes, activation=last_layer_activation)(layer)
+
+  model=Model(inputs=visible,outputs=layer)
+
+  # plot_model(model, show_shapes=True, to_file='model.png')
   return model
 
+if __name__ == '__main__':
+  create_model()
