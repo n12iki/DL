07b1ed8e20de8918376002296ebdc83a46dc8cbf
Demetre Dzmanashvili <demetredzmanashvili@gmail.com>
2022-05-16 17:48:30 +0200

    renamed folder name

---
+++ transfer_learning/launcher.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#SBATCH --job-name="mame"
+
+#SBATCH --qos=training
+
+#SBATCH --output=mame_%j.out
+
+#SBATCH --error=mame_%j.err
+
+#SBATCH --cpus-per-task=40
+
+#SBATCH --gres gpu:1
+
+#SBATCH --time=08:00:00
+
+module purge; module load gcc/8.3.0 ffmpeg/4.2.1 cuda/10.2 cudnn/7.6.4 nccl/2.4.8 tensorrt/6.0.1 openmpi/4.0.1 atlas/3.10.3 scalapack/2.0.2 fftw/3.3.8 szip/2.1.1 opencv/4.1.1 python/3.7.4_ML
+
+
+mkdir $SLURM_JOBID
+
+cp data_loader.py $SLURM_JOBID
+cp model.py $SLURM_JOBID
+cp trainer.py $SLURM_JOBID
+
+python trainer.py -$SLURM_JOBID
+
+mv "mame_$SLURM_JOBID.out" $SLURM_JOBID
+mv "mame_$SLURM_JOBID.err" $SLURM_JOBID
---
+++ transfer_learning/model.py
@@ -0,0 +1,42 @@
+from statistics import mode
+import keras as k
+from tensorflow.keras.models import Model
+from keras.models import Sequential
+from keras import regularizers
+from keras.layers import Dense, Activation, Flatten, Dropout, BatchNormalization, GlobalAveragePooling2D
+from keras.layers import Conv2D, MaxPooling2D
+from tensorflow.keras import optimizers
+from data_loader import MAMeDataset
+import matplotlib.pyplot as plt
+from keras import applications
+
+IMG_SIZE = 256
+NUM_CLASSES = 20
+
+def create_model():
+  model = applications.resnet50.ResNet50(
+    include_top=False,
+    weights='imagenet',
+    input_shape=(IMG_SIZE, IMG_SIZE, 3),
+    pooling=None,
+    classes=NUM_CLASSES
+  )
+
+  # example of freezing layers
+  for layer in model.layers[:170]:
+    layer.trainable = False
+
+  
+  # custom model example
+  #Adding custom Layers 
+  x = model.output
+  x = Flatten()(x)
+  x = Dense(512, activation="relu")(x)
+  x = Dropout(0.2)(x)
+  x = Dense(512, activation="relu")(x)
+  predictions = Dense(NUM_CLASSES, activation="softmax")(x)
+
+  # creating the final model 
+  model_final = Model(model.input, predictions)
+
+  return model_final
