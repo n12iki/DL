aa0d55c8685b52d669ff7a30d5dd8a173d2d5f14
n12iki <p12ain@gmail.com>
2022-05-17 02:32:37 +0200

    added back early stop

--- transfer_learning/trainerUnfreeze.py
+++ transfer_learning/trainerUnfreeze.py
@@ -23,12 +23,12 @@ def train():
     acc=[]
     val_acc=[]
     # early_stopping_monitor = EarlyStopping(monitor='val_accuracy', min_delta=0, patience=10, verbose=1, mode='auto')
-    early_stopping_monitor = EarlyStopping(patience=10)
+    early_stopping_monitor = EarlyStopping(patience=15,monitor='val_accuracy',mode="max")
     opt_rms = optimizers.RMSprop(learning_rate=0.0001, decay=1e-6)
     model.compile(loss=loss, optimizer=opt_rms, metrics=['acc'])
     mdl_fit = model.fit_generator(
         train_dataset, steps_per_epoch=len(train_dataset),
-        # , callbacks=[early_stopping_monitor]
+        callbacks=[early_stopping_monitor],
         epochs=n_epochs, verbose=1, validation_data=test_dataset
     )
 
